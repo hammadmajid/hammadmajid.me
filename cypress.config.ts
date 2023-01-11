a156cac8623b066c204d64fb2f6c6dbce8f5772b
Hammad Majid <hm.hammadmajid@gmail.com>
2023-01-11 15:32:23 +0500

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectID: "361bhh",
+  projectId: "361bhh",
   e2e: {
     baseUrl: "http://localhost:3000/",
     supportFile: false,
