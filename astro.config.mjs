d18b8af0a5682abf0217c01019f9fb34fc886d1a
hammadmajid <hm.hammadmajid@gmail.com>
2023-02-18 13:03:22 +0000

    Add site link for site-map

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://hammadmajid.com',
   integrations: [tailwind(), sitemap()]
 });
