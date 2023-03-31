67a0b1284dd68c9d1e378e1c82f81b483b05d0ea
Hammad Majid <hammadmajid@pm.me>
2023-03-31 17:16:15 +0500

    Format code with added prettier config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,22 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
-import partytown from "@astrojs/partytown";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
-import prefetch from "@astrojs/prefetch";
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hammadmajid.com',
-  integrations: [tailwind(), sitemap(), svelte(), partytown(), prefetch()]
+  integrations: [tailwind(), sitemap(), svelte(), partytown(), prefetch()],
 });
