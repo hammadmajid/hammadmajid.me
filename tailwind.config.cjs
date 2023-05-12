95bc7015bb9d5b208868224404b2dd84c8ae7950
Hammad Majid <hm.hammadmajid@gmail.com>
2023-05-12 22:03:37 +0500

    use Atkinson-Hyperlegible font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Atkinson-Hyperlegible': ['Atkinson-Hyperlegible', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 };
