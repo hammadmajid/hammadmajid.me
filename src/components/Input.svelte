f7f19f5e1ec77e98299252c324db6c530428ec2d
Hammad Majid <hm.hammadmajid@gmail.com>
2023-05-13 20:06:35 +0500

    Create Input.svelte

---
+++ src/components/Input.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let name: string;
+  export let type: 'text' | 'textarea' | 'email';
+  export let placeholder: string;
+  export let label: string;
+  export let required: boolean;
+</script>
+
+<label for={label}>
+  {#if type === 'text' || type === 'email'}
+    <input
+      {type}
+      {required}
+      {placeholder}
+      {name}
+      class="p-2 rounded-sm outline outline-2 outline-slate-800"
+    />
+  {:else if type === 'textarea'}
+    <textarea
+      {name}
+      {required}
+      {placeholder}
+      class="p-2 rounded-sm outline outline-2 outline-slate-800"
+    />
+  {/if}
+</label>
