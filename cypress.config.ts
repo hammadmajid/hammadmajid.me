7ddd816a39a0b0b96d81ed755fa428a2c22a6eb3
Hammad Majid <hm.hammadmajid@gmail.com>
2023-05-14 21:45:30 +0500

    add cypress.config.ts to

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "ayg98x",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
