08d79438034d991398669652b62799fff02d5df2
Hammad Majid <hm.hammadmajid@gmail.com>
2023-05-14 22:26:17 +0500

    create support for e2e testing

---
+++ cypress/support/e2e.ts
@@ -0,0 +1,15 @@
+// ***********************************************************
+// This support/e2e.ts is processed and loaded
+// automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+/**
+ * The url to visit before each test
+ */
+const baseURL = 'http://localhost:3000';
