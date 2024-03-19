506d892290562c684c2b0b42b0982e51fb7750f9
Hammad Majid <hammadmajid@proton.me>
2024-03-20 00:02:17 +0500

    use emailjs client to implement form submission send logic

--- src/pages/api/form/send.ts
+++ src/pages/api/form/send.ts
@@ -1,16 +1,56 @@
-import type { APIRoute } from "astro";
+import type { APIRoute } from 'astro';
+import { SMTPClient } from 'emailjs';
 
 export const POST: APIRoute = async ({ request }) => {
+  const client = new SMTPClient({
+    user: import.meta.env.EMAILJS_CLIENT_USER,
+    password: import.meta.env.EMAILJS_CLIENT_PASSWORD,
+    host: import.meta.env.EMAILJS_CLIENT_HOST,
+    tls: {
+      ciphers: 'SSLv3',
+    },
+  });
+
   const data = await request.formData();
-  const name = data.get("name");
-  const email = data.get("email");
-  const message = data.get("message");
+  const name = data.get('name');
+  const email = data.get('email');
+  const message = data.get('message');
+
+  let sendErr;
+  const messageBody: string = `
+  name: ${name}\n
+  email: ${email}\n
+  
+  ${message}
+  `;
+
+  client.send(
+    {
+      from: import.meta.env.EMAILJS_SEND_FROM,
+      to: import.meta.env.EMAILJS_SEND_TO,
+      subject: 'hammadmajid.me | New form submission',
+      text: `${messageBody}`,
+    },
+    (err) => {
+      sendErr = err;
+    },
+  );
+
+  if (sendErr) {
+    return new Response(
+      JSON.stringify({
+        message: "Failed to submit form.",
+        status: 500,
+      }),
+      { status: 500 },
+    );
+  }
 
   return new Response(
     JSON.stringify({
-      message: "Form submission not implemented yet.",
-      status: 500
+      message: "Form submitted successfully!",
+      status: 200,
     }),
-    { status: 500 }
+    { status: 200 },
   );
 };
