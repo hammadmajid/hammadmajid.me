ab8e0ccdd2ccd31024f462160e90334d92d481dc
Hammad Majid <hammadmajid@proton.me>
2024-03-25 04:07:20 +0500

    add rss icon

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,6 +1,7 @@
 const icons = {
   barsBottomRight: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" /></svg>`,
   xMark: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="w-6 h-6 stroke-2"><path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" /></svg>`,
+  rss: `<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 fill-light-accent"><path d="M19 20.001C19 11.729 12.271 5 4 5v2c7.168 0 13 5.832 13 13.001h2z"></path><path d="M12 20.001h2C14 14.486 9.514 10 4 10v2c4.411 0 8 3.589 8 8.001z"></path><circle cx="6" cy="18" r="2"></circle></svg>`,
 };
 
 export default icons;
